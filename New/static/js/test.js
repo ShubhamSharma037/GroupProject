ab45d48c7786dd4a1b8a022d59fb2cfc2a11bfab
SS9987 <frovi.ga@gmail.com>
2019-07-17 22:33:25 +0530

    Project Page Tweaks

---
+++ New/static/js/test.js
@@ -0,0 +1,22 @@
+$(document).ready(function () {
+    size_li = $("#myList .project").size();
+    x=8;
+    $('#myList .project:lt('+x+')').show();
+    $('#loadMore').click(function () {
+        x= (x+4 <= size_li) ? x+4 : size_li;
+        $('#myList .project:lt('+x+')').show();
+         $('#showLess').show();
+        if(x == size_li){
+            $('#loadMore').hide();
+        }
+    });
+    $('#showLess').click(function () {
+        x=(x-4<0) ? 8 : x-4;
+        $('#myList .project').not(':lt('+x+')').hide();
+        $('#loadMore').show();
+         $('#showLess').show();
+        if(x == 8){
+            $('#showLess').hide();
+        }
+    });
+});
